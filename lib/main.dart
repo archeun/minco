09c05606f5352bbde03e336cf0ac1e79c8ea5add
Aruna Tebel <arunautebel@gmail.com>
2023-12-03 23:18:20 +0530

    Cleanup the references and usage of firebase and firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,6 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'app.dart';
-import 'firebase_options.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  if (kDebugMode) {
-    try {
-      FirebaseFirestore.instance.useFirestoreEmulator('10.0.2.2', 8080);
-    } catch (e) {
-      print(e);
-    }
-  }
   runApp(const MincoApp());
 }
